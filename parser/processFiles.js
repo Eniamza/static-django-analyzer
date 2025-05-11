d5d74d5dfdbe0f2230aebe7436cd6da7a3351ad6
eniamza <eniamza0@gmail.com>
2025-05-11 16:52:04 +0600

    feat: Categorization of path arguments in the root urls file

--- parser/processFiles.js
+++ parser/processFiles.js
@@ -67,9 +67,8 @@ return settingRelatedFilesPath
 
 }
 
-function getInstalledApps (ast) {
-    let rootNode = ast.rootNode
-
+function getInstalledApps(ast) {
+    let rootNode = ast.rootNode;
     let defaultInstalledApps = [
         'django.contrib.admin',
         'django.contrib.auth',
@@ -79,66 +78,117 @@ function getInstalledApps (ast) {
         'django.contrib.staticfiles',
         'rest_framework',
         'django_filters'
-    ]
+    ];
 
-    let allExpressionStatements = rootNode.descendantsOfType('expression_statement')
+    let allExpressionStatements = rootNode.descendantsOfType('expression_statement');
     let installedApps = [];
+
     for (const expression of allExpressionStatements) {
         let expressionText = expression.text;
         if (expressionText.includes("INSTALLED_APPS")) {
-            let appsText = expressionText.split('=')[1].trim();
-
-            appsText = appsText
-                .replace(/^\[|\]$/g, '')
-                .split(',')
-                .map(app => app.trim())
-                .map(app => app.replace(/['"]/g, ''))
-                .filter(app => app.length > 0);
-
-            installedApps = appsText;
-            installedApps = installedApps.filter(app => !defaultInstalledApps.includes(app));
-            console.log('Installed apps found:', installedApps);
-            break; // Exit the loop once we find INSTALLED_APPS
+            let listDecendant = expression.descendantsOfType('list')[0]
+            let strings = listDecendant.descendantsOfType('string')
+            strings.forEach((installed) => {
+                const cleanText = installed.text.slice(1, -1)
+                if (!defaultInstalledApps.includes(cleanText)) {
+                    installedApps.push(cleanText);
+                }
+            });
         }
     }
+
+    console.log('Installed Apps:', installedApps);
 
     if (installedApps.length === 0) {
         throw new Error('No INSTALLED_APPS found in the settings file.');
     }
 
     return installedApps;
-
 }
 
-function getRootUrls (ast,installedApps) {
-    let rootNode = ast.rootNode
+function getRootUrls(ast, installedApps) {
+    let rootNode = ast.rootNode;
 
     // Fetch all "path" and "re_path" function calls
-    let allCallFunctions = rootNode.descendantsOfType('call')
+    let allCallFunctions = rootNode.descendantsOfType('call');
+    let allUrls = [];
+    let allPathCallNodes = [];
 
-
-
-    let allUrls = []
     allCallFunctions.forEach((call) => {
-        let functionName = call.firstNamedChild.text
+        let functionName = call.firstNamedChild.text;
         if (functionName === "path" || functionName === "re_path") {
-            let urlPath = call.firstNamedChild.nextNamedSibling.text
-            allUrls.push(urlPath)
+            allPathCallNodes.push(call);
         }
-    })
-
-    // Filter URLs based on installed apps
-    let filteredUrls = allUrls.filter(url => {
-        return installedApps.some(app => url.includes(app));
     });
 
-    if (filteredUrls.length === 0) {
-        console.log('No URLs found for the installed apps.');
-        throw new Error('No URLs found for the installed apps.');
-    } else {
-        console.log('Filtered URLs found:', filteredUrls);
-        return filteredUrls
+    let pathCallandArgumentsMap = {}
+
+    // console.log('All path and re_path calls:', allPathCallNodes[4].children[1].children.forEach((child) => {
+    //     console.log('child:', child.text);
+    // }
+    // ));
+
+    for (const pathCall of allPathCallNodes) {
+        let arguments = pathCall.namedChildren[1];
+        let pathName = arguments.descendantsOfType('string')[0].text.slice(1, -1);
+        pathCallandArgumentsMap[pathName] = []
+        let pathArguments = []
+
+        arguments.namedChildren.forEach((arg) => {
+            pathArguments.push(arg)
+        }
+        )
+
+        pathCallandArgumentsMap[pathName] = pathArguments
+        
     }
+
+    let pathCallandResolvedArgumentsMap = {}
+
+    for (const [key, value] of Object.entries(pathCallandArgumentsMap)) {
+        console.log(`Processing key: ${key}`);
+
+        let buildPathArgumentObject = {
+            route: null,
+            view: null,
+            kwargs: null,
+            name: null
+        }
+
+        for (const [index, arg] of value.entries()) {
+            // If the index is 0 and it's not a keyword argument, it's the route
+            if (index === 0 && arg.type !== 'keyword_argument') {
+                console.log('route:', arg.text);
+            }
+            // If the index is 1 and it's not a keyword argument, it's the view
+            else if (index === 1 && arg.type !== 'keyword_argument') {
+                console.log('view:', arg.text);
+            }
+            // If the index is 2 and it's a dictionary type, it's the kwargs
+            else if (index === 2 && arg.type === 'dictionary') {
+                console.log('kwargs:', arg.text);
+            }
+            // If the index is 3 and it's not a keyword argument, it's the name
+            else if (index === 3 && arg.type !== 'keyword_argument') {
+                console.log('name:', arg.text);
+            }
+            // If it's a keyword argument, check its name and assign it accordingly
+            else if (arg.type === 'keyword_argument') {
+                // let keywordName = arg.firstNamedChild.text;
+                // let keywordValue = arg.lastNamedChild.text;
+
+                // if (keywordName === 'kwargs') {
+                //     buildPathArgumentObject.kwargs = keywordValue;
+                // } else if (keywordName === 'name') {
+                //     buildPathArgumentObject.name = keywordValue.slice(1, -1);
+                // }
+                console.log('keyword argument:', arg.text);
+            }
+      }
+    
+    }
+    // console.log('All path and re_path calls:', pathCallandArgumentsMap);
+    return pathCallandArgumentsMap;
 }
 
 module.exports = { processArguments, checkArguments, getSettingsFile, getInstalledApps , getRootUrls }
